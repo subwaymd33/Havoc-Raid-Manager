0fb140e63724651fac22f66e6b7055b5afa01f0f
subwaymd33 <michael.dilernia@gmail.com>
2022-09-11 13:29:29 -0400

    added code for logon guard

--- WoWRaidManager/src/app/shared/guards/logon-guard.ts
+++ WoWRaidManager/src/app/shared/guards/logon-guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import {  CanActivate } from "@angular/router";
+import { ConnectableObservable } from "rxjs";
 import { UserAuthService } from "src/app/services/userAuth.service";
 
 
@@ -13,8 +14,14 @@ export class LogonGuardService implements CanActivate {
   constructor(private userAuthService: UserAuthService) { }
 
   canActivate(): boolean {
-
-    return this.userAuthService.isLoggedIn();
+    console.log("checking activate")
+    try {
+      let isloggedin = this.userAuthService.isLoggedIn()
+      console.log(isloggedin)
+      return isloggedin
+    } catch (error) {
+      return false
+    }
   }
 
 }
