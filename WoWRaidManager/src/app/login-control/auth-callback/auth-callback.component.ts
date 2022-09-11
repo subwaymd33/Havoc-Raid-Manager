04023a8d948c105f17dcc26a8ffd0f9ce245fe2c
subwaymd33 <michael.dilernia@gmail.com>
2022-09-11 14:43:53 -0400

    remove officer discord check

--- WoWRaidManager/src/app/login-control/auth-callback/auth-callback.component.ts
+++ WoWRaidManager/src/app/login-control/auth-callback/auth-callback.component.ts
@@ -18,7 +18,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AuthCallbackComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,private _router:Router, private authService: UserAuthService, private userService: UserService, private sessionService: SessionService, private cookieService: CookieService) { }
+  constructor(private route: ActivatedRoute, private _router: Router, private authService: UserAuthService, private userService: UserService, private sessionService: SessionService, private cookieService: CookieService) { }
 
   ngOnInit(): void {
     console.log("entered auth callback")
@@ -26,71 +26,67 @@ export class AuthCallbackComponent implements OnInit {
     var userData: discordUserData;
     var access: accessToken;
 
-    this.authService.GetAccessToken(discordCode.toString()).subscribe((retAccessToken: accessToken) => {
+    this.authService.GetAccessToken(discordCode.toString())
+    .subscribe((retAccessToken: accessToken) => {
       access = retAccessToken
       this.authService.AuthenticateUser(access.access_token.toString()).subscribe((data: discordUserData) => {
-        console.log("Got Past authenticate user")
+        // console.log("Got Past authenticate user")
         userData = data
-   
+
         //this.cookieService.set("isLogon", "true")
-        this.cookieService.set("username", userData.username)
-        this.cookieService.set("isLogon", "true")
-        console.log("Set islogon and username cookies")
-        this.authService.GetDiscordGuilds(access.access_token.toString()).subscribe((data) => {
-          console.log("Getdiscordguild check complete. moving to processing")
-          data.forEach(guild => {
-            console.log("Discord guild code:" + guild)
-            if (guild.id == '822219949284130839') {
-              console.log("Discord Guild Id matched Panic")
-              this.authService.GetDiscordGuildsDetails(access.access_token.toString(), guild.id).subscribe(guildData => {
-                var discordRolesArray: DiscordGuildDetails = guildData
-                discordRolesArray.roles.forEach((role: string) => {
-                  console.log("Looping Roles: "+ role)
-                  if (role == "825857440796508191") {
-                    this.authService.SetAdmin(true)
-                    userData.role = "officer"
-                  }
-                })
-                this.userService.CheckUser(userData.id).subscribe(checkdata => {
-                  if (checkdata.length == 0) {
-                    //insert new user
 
-                    this.userService.AddUser(userData).subscribe(data => {
-                    })
-                  }
+        // console.log("Set islogon and username cookies")
+        // this.authService.GetDiscordGuilds(access.access_token.toString()).subscribe((data) => { });
+        // console.log("Getdiscordguild check complete. moving to processing")
+        // data.forEach(guild => {
+        //   console.log("Discord guild code:" + guild)
+        //   if (guild.id == '822219949284130839') {
+        //     console.log("Discord Guild Id matched Panic")
+        //     this.authService.GetDiscordGuildsDetails(access.access_token.toString(), guild.id).subscribe(guildData => {
+        //       var discordRolesArray: DiscordGuildDetails = guildData
+        //       discordRolesArray.roles.forEach((role: string) => {
+        //         console.log("Looping Roles: "+ role)
+        //         if (role == "825857440796508191") {
+        //           this.authService.SetAdmin(true)
+        //           userData.role = "officer"
+        //         }
+        //       })
+        this.userService.CheckUser(userData.id).subscribe(checkdata => {
+          if (checkdata.length == 0) {
+            //insert new user
+            this.userService.AddUser(userData).subscribe(data => {
+            })
+          }else{
+            if (checkdata[0].role =="officer"){
+              this.authService.SetAdmin(true)
+            }
+          }
 
-                  this.cookieService.set("dID", userData.id)
-                  this.cookieService.set("username", userData.username)
+          this.cookieService.set("isLogon", "true")
+          this.cookieService.set("dID", userData.id)
+          this.cookieService.set("username", userData.username)
 
-                  //localStorage.setItem("dID", userData.id);
-                  //localStorage.setItem("username", userData.username)
-                  this.authService.SetLogon(true);
+          //localStorage.setItem("dID", userData.id);
+          //localStorage.setItem("username", userData.username)
+          this.authService.SetLogon(true);
 
-                  var exp_date = new Date()
-                  exp_date.setSeconds(exp_date.getSeconds() + access.expires_in)
-                  var currentSession = new SessionModel(userData.id, access.access_token, exp_date, access.refresh_token)
+          var exp_date = new Date()
+          exp_date.setSeconds(exp_date.getSeconds() + access.expires_in)
+          var currentSession = new SessionModel(userData.id, access.access_token, exp_date, access.refresh_token)
 
-                  this.sessionService.CheckSession(currentSession).subscribe(sessionCheckData => {
-                    if (sessionCheckData.length == 0) {
-                      //if no item was found just add new
-                      this.sessionService.AddSession(currentSession).subscribe(data => {
-                      })
-                    } else {
-                      //update DB row with new values from full auth flow
-                      this.sessionService.UpdateSession(currentSession).subscribe(data => {
-                      })
-                    }
-                  })
-            
-                  this._router.navigate(['/userPage'])
-                
-                })
+          this.sessionService.CheckSession(currentSession).subscribe(sessionCheckData => {
+            if (sessionCheckData.length == 0) {
+              //if no item was found just add new
+              this.sessionService.AddSession(currentSession).subscribe(data => {
+              })
+            } else {
+              //update DB row with new values from full auth flow
+              this.sessionService.UpdateSession(currentSession).subscribe(data => {
               })
             }
           })
+          this._router.navigate(['/userPage'])
         })
       })
-    })
+    })}
   }
-
-}
--- WoWRaidManager/src/app/login-control/models/userCheckModel.ts
+++ WoWRaidManager/src/app/login-control/models/userCheckModel.ts
@@ -1,7 +1,9 @@
 export class userCheckModel{
     user_name:string;
+    role:string;
 
-    constructor(user_name:string){
+    constructor(user_name:string, role:string){
         this.user_name = user_name;
+        this.role=role;
     }
 }
